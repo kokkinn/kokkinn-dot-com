cb18a8eb09d46fdf86cd4504b64787b38a40890b
George Kokkin <george@petproactive.pet>
2024-05-10 22:11:20 +0100

    Changes

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const lineDensity = 10;
+const speed = 80;
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.body.appendChild(canvas);
+
+function generateNoise() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let x = 0; x < canvas.width; x += lineDensity) {
+        if (Math.random() < 0.5) {
+            const startY = -canvas.height;
+            ctx.beginPath();
+            ctx.moveTo(x, canvas.height * Math.random());
+            ctx.lineTo(x, startY);
+            ctx.strokeStyle = 'rgba(165,165,165,0.15)';
+            ctx.stroke();
+        }
+    }
+}
+
+generateNoise();
+
+function updateNoise() {
+    setInterval(() => {
+        generateNoise();
+    }, speed); // Adjust the speed as needed
+}
+
+updateNoise();
